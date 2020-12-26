1c3c091f48e157b5c89436e29c0316f62dace88a
VarytDev <bartex220@gmail.com>
2020-12-26 14:29:03 +0100

    Initial commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static TextureGenerator;
+
+//Made with tutorial: https://www.youtube.com/watch?v=wbpMiKiSKm8&list=PLFt_AvWsXl0eBW2EiBtl_sxmDtSgZBxB3
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode
+    {
+        NoiseMap,
+        ColorMap,
+        Mesh
+    }
+    public DrawMode drawMode;
+
+    public int mapWidth, mapHeight;
+    public float noiseScale;
+    public int octaves;
+    [Range(0, 1)] public float persistance;
+    public float lacunarity;
+    public int seed;
+    public Vector2 offset;
+
+    public float heightMultiplier;
+    public AnimationCurve meshHeightCurve;
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+        
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+
+                for(int i = 0; i < regions.Length; i++)
+                {
+                    if(currentHeight <= regions[i].height)
+                    {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        MapDisplay mapDisplay = FindObjectOfType<MapDisplay>();
+        if(drawMode == DrawMode.NoiseMap) mapDisplay.DrawTexture(TextureFromHeightMap(noiseMap));
+        else if(drawMode == DrawMode.ColorMap) mapDisplay.DrawTexture(TextureFromColorMap(colorMap, mapWidth, mapHeight));
+        else if(drawMode == DrawMode.Mesh) mapDisplay.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, heightMultiplier, meshHeightCurve), TextureFromColorMap(colorMap, mapWidth, mapHeight));
+    }
+
+    void OnValidate()
+    {
+        //clamping values in inspector
+        mapWidth = (mapWidth < 1 ? 1 : mapWidth);
+        mapHeight = (mapHeight < 1 ? 1 : mapHeight);
+
+        lacunarity = (lacunarity < 1 ? 1 : lacunarity);
+        octaves = (octaves < 0 ? 0 : octaves);
+    }
+
+    
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
+    {
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        //calculating offset for map samples based on seed and manual offset
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for(int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-10000, 10000) + offset.x; //or 100,000
+            float offsetY = prng.Next(-10000, 10000) + offset.y; //or 100,000
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        //clamping scale to minimum value of 0.0001
+        if(scale <= 0) scale = 0.0001f;
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        //calculate center of noise map
+        float halfWidth = mapWidth / 2;
+        float halfHeight = mapHeight / 2;
+
+        //first two loops go through every point on the map
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                //generating perlin noise value for each point, and layering octaves
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for(int i = 0; i < octaves; i++)
+                {    
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                //setting min and max noiseHeight
+                maxNoiseHeight = (noiseHeight > maxNoiseHeight ? noiseHeight : maxNoiseHeight);
+                minNoiseHeight = (noiseHeight < minNoiseHeight ? noiseHeight : minNoiseHeight);
+
+                noiseMap[x, y] = noiseHeight;
+            }
+        }
+
+        //normalizing noiseHeight in every point
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+        }
+
+        return noiseMap;
+    }
+}
